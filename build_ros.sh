46298fc5be77264407158da828b0e3ac301d723d
sunfu-chou <sunfu.chou@gmail.com>
2022-07-05 12:40:53 +0800

    Update ros build dependencies

--- build_ros.sh
+++ build_ros.sh
@@ -1,6 +1,12 @@
 echo "Building ROS nodes"
 
-cd Examples/ROS/ORB_SLAM3
+cd Examples_old/ROS/ORB_SLAM3
+source /opt/ros/noetic/setup.bash
+export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:/home/dit/ORB_SLAM3/Examples_old/ROS/ORB_SLAM3
+rosdep init
+rosdep update
+sed -i 's/++11/++14/g' CMakeLists.txt
+
 mkdir build
 cd build
 cmake .. -DROS_BUILD_TYPE=Release
