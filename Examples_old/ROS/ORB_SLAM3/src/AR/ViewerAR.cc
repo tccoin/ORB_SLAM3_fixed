12b4d9969aef2fb34fe63433f491c078ac7e86d8
sunfu-chou <sunfu.chou@gmail.com>
2022-07-05 19:43:07 +0800

    Update ros_mono_ar.cc: fix issue #442

--- Examples_old/ROS/ORB_SLAM3/src/AR/ViewerAR.cc
+++ Examples_old/ROS/ORB_SLAM3/src/AR/ViewerAR.cc
@@ -19,6 +19,8 @@
 #include "ViewerAR.h"
 
 #include <opencv2/highgui/highgui.hpp>
+#include <Eigen/Dense>
+#include <opencv2/core/eigen.hpp>
 
 #include <mutex>
 #include <thread>
@@ -402,7 +404,10 @@ Plane* ViewerAR::DetectPlane(const cv::Mat Tcw, const std::vector<MapPoint*> &vM
         {
             if(pMP->Observations()>5)
             {
-                vPoints.push_back(pMP->GetWorldPos());
+                // vPoints.push_back(pMP->GetWorldPos());
+                cv::Mat WorldPos;
+                cv::eigen2cv(pMP->GetWorldPos(), WorldPos);
+                vPoints.push_back(WorldPos);
                 vPointMP.push_back(pMP);
             }
         }
@@ -527,7 +532,9 @@ void Plane::Recompute()
         MapPoint* pMP = mvMPs[i];
         if(!pMP->isBad())
         {
-            cv::Mat Xw = pMP->GetWorldPos();
+            // cv::Mat Xw = pMP->GetWorldPos();
+            cv::Mat Xw;
+            cv::eigen2cv(pMP->GetWorldPos(), Xw);
             o+=Xw;
             A.row(nPoints).colRange(0,3) = Xw.t();
             nPoints++;
